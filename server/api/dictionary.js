f711d2535a910755f9cebc1436abb6e640a40eb4
Elijah Sciammas <eli.sciammas@gmail.com>
2021-05-03 15:45:06 -0400

    Somewhat confused bu persevering...

--- server/api/dictionary.js
+++ server/api/dictionary.js
@@ -3,8 +3,11 @@
 const fs = require('fs')
 const router = require('express').Router()
 const readData = require('./dict/parser')
+const https = require('https')
 
 module.exports = router
+const REGEX_CHINESE = /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u
+const isChinese = str => REGEX_CHINESE.test(str)
 
 const read = async () => {
   let dict = await fs.readFileSync(
@@ -23,13 +26,18 @@ const ChineseDictionary = function(obj) {
   }
 }
 
-ChineseDictionary.prototype.find = async function(word) {
-  const dict = await read()
-}
-
-// const buildRequest = (options) => {
-//   router.get()
+// ChineseDictionary.prototype.find = function(word) {
+//   return buildRequest(word, this.config.char_type)
 // }
+
+// const buildRequest = (word, style) => {
+//  if (isChinese(word)) {
+//  }
+// }
+
+function OptionObj(path) {
+  const options = {}
+}
 
 router.get('/', async (req, res, next) => {
   let dict = await fs.readFileSync(
