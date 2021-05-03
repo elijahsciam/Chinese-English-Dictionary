db46ef8bb73129391f9ff071675c30b0f8f7b620
Elijah Sciammas <eli.sciammas@gmail.com>
2021-05-03 14:38:58 -0400

    Feat: Beginning development of NPM Chinese-Eng dictionary

--- server/api/dictionary.js
+++ server/api/dictionary.js
@@ -1,8 +1,35 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
 const fs = require('fs')
 const router = require('express').Router()
 const readData = require('./dict/parser')
 
 module.exports = router
+
+const read = async () => {
+  let dict = await fs.readFileSync(
+    'server/api/dict/cedict_ts.u8',
+    'utf8',
+    (err, data) => {
+      if (err) throw err
+    }
+  )
+  return dict
+}
+
+const ChineseDictionary = function(obj) {
+  this.config = {
+    char_type: obj.character || 'simplified'
+  }
+}
+
+ChineseDictionary.prototype.find = async function(word) {
+  const dict = await read()
+}
+
+// const buildRequest = (options) => {
+//   router.get()
+// }
 
 router.get('/', async (req, res, next) => {
   let dict = await fs.readFileSync(
