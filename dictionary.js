270a3fe3efae8d24ed61cbaafc97653fb919a1c5
Elijah Sciammas <eli.sciammas@gmail.com>
2021-05-17 16:09:47 -0400

    Bug: figuring out https server to enable proper npm installation

--- dictionary.js
+++ dictionary.js
@@ -2,15 +2,23 @@
 /* eslint-disable no-unused-vars */
 const fs = require('fs');
 const parseData = require('./dict/parser');
+const https = require('https');
 
 const REGEX_CHINESE = /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u;
 const isChinese = (str) => REGEX_CHINESE.test(str);
 
-const buildRequest = (word, style, language) => {
-  let dict = fs.readFileSync('./dict/cedict_ts.u8', 'utf8', (err, data) => {
-    if (err) throw err;
-  });
+const endpoint =
+  'https://raw.githubusercontent.com/elijahsciam/Chinese-English-Dictionary/npm/dict/cedict_ts.u8';
 
+let dict = https
+  .request(endpoint, (res) => {
+    res.on('data', (d) => {
+      process.stdout.write(d);
+    });
+  })
+  .end();
+
+const buildRequest = (word, style, language) => {
   const dictionary = parseData(dict);
   if (language === 'chinese') {
     if (style === 'simplified') {
